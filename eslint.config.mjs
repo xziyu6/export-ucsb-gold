4f1cf4d08189ab75688377e49f6ed7feb5567c59
Chazz Yu <chazzyu6@gmail.com>
2025-11-19 01:43:46 -0800

    additional config, use prettier and eslint instead of standard

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended"
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+  },
+  eslintConfigPrettier,
+  eslintPluginPrettierRecommended,
+]);
