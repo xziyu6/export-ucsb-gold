355a1a00f99089137f13bbbd229f5a2ef8fce823
Chazz Yu <chazzyu6@gmail.com>
2025-11-20 02:16:46 -0800

    save_quarter_info debugged

--- scripts/save_quarter_info.js
+++ scripts/save_quarter_info.js
@@ -1,5 +1,4 @@
-import dayjs from "dayjs";
-import $, {get} from "jquery";
+// FIXME: runs on previous quarter page when scrolldown element changes
 
 // Function to save quarter's start and end dates to Chrome storage
 function saveDates(quarterInfo) {
@@ -16,7 +15,7 @@ function saveDates(quarterInfo) {
     };
 
     // save quarter info
-    chrome.storage.local.set({quarters: quarters}, function() {
+    chrome.storage.local.set({quarters : quarters}, function() {
       if (chrome.runtime.lastError) {
         console.error("Error saving quarter info:", chrome.runtime.lastError);
       } else {
@@ -24,7 +23,7 @@ function saveDates(quarterInfo) {
       }
     });
     // set currentQuarter to quarterId of saved quarter
-    chrome.storage.local.set({currentQuarter: quarterId}, function() {
+    chrome.storage.local.set({currentQuarter : quarterId}, function() {
       if (chrome.runtime.lastError) {
         console.error("Error saving current quarter:", chrome.runtime.lastError);
       } else {
@@ -36,19 +35,19 @@ function saveDates(quarterInfo) {
 
 // Format date from MM/DD/YYYY to YYYY-MM-DD using day.js
 function formatDate(dateString) {
-  return dayjs(dateString, "M/D/YYYY").format("YYYY-MM-DD");
+  const [month, day, year] = dateString.split('/').map(Number);
+  return `${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`;
 }
 
 // Retrieves information about the current quarter, including its ID, text, start date, and end date.
 function getQuarterInfo() {
   const startDate = formatDate(
-          $("#pageContent_FirstDayInstructionLabel").text().trim(),
-  );
+          $("#pageContent_FirstDayInstructionLabel").text().trim());
   const endDate = formatDate(
-          $("#pageContent_LastDayInstructionLabel").text().trim(),
-  );
-  const quarter = $("#pageContent_quarterDropDown option:selected");
-  const quarterId = quarter.value.trim();
+          $("#pageContent_LastDayInstructionLabel").text().trim());
+  const quarter = $("#pageContent_quarterDropDown option[selected='selected']");
+  console.log(quarter.attr("value"));
+  const quarterId = quarter.attr("value").trim();
   const quarterText = quarter.text().trim();
   return {
     quarterId : quarterId,
